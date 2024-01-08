65bdffc5cd958ebc5ea0f3ba1213999758677feb
michaelshampaner <154332587+michaelshampaner@users.noreply.github.com>
2024-01-08 11:56:53 +0200

    Update Calendar1.java
    without breakpoint

--- Calendar1.java
+++ Calendar1.java
@@ -17,7 +17,7 @@ public class Calendar1 {
 		// Advances the date and the day-of-the-week from 1/1/1900 till 31/12/1999, inclusive.
 	    // Prints each date dd/mm/yyyy in a separate line. If the day is a Sunday, prints "Sunday".
 	    // The following variable, used for debugging purposes, counts how many days were advanced so far.
-	    int debugDaysCounter = 0; 
+	   // int debugDaysCounter = 0; 
 	    //// Write the necessary initialization code, and replace the condition
 	    //// of the while loop with the necessary condition 
 	 	int numOfSundays = 0;
@@ -33,11 +33,11 @@ public class Calendar1 {
 				System.out.println(dayOfMonth + " / " + month + " / " + year);	
 			}
 	 		advance();
-	 		debugDaysCounter++;
+	 		//debugDaysCounter++;
 	 		//// If you want to stop the loop after n days, replace the condition of the
 	 		//// if statement with the condition (debugDaysCounter == n)
-	 		if (debugDaysCounter == 3000) { 
-	 			break;}
+	 		//if (debugDaysCounter == 3000) { 
+	 			//break;}
         }
         System.out.println("During the 20th century, " + numOfSundays + " Sundays fell on the first day of the month"); 
 	 }
